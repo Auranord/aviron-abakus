555a478ea43ceeefeac172f2e65ba4d485595c7c
auranord <aura.nord@googlemail.com>
2022-04-18 16:47:41 +0200

    First commit.

---
+++ src/main/java/de/aviron/abacus/AvironAbacusApplication.java
@@ -0,0 +1,13 @@
+package de.aviron.abacus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvironAbacusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvironAbacusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/aviron/abacus/AvironAbacusApplicationTests.java
@@ -0,0 +1,13 @@
+package de.aviron.abacus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvironAbacusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
