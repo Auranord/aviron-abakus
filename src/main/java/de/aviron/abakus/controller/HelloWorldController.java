934b4566e0289d66883fdd3bd64a64c325e0829f
auranord <aura.nord@googlemail.com>
2022-04-19 11:59:24 +0200

    Test Output on /.

---
+++ src/main/java/de/aviron/abakus/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package de.aviron.abakus.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+}
