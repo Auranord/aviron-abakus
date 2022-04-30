5fb234bf4c635371042fd25c68adfed9ca320c6c
auranord <aura.nord@googlemail.com>
2022-04-30 15:27:59 +0200

    Added RequestMapping to FigureController

--- src/main/java/de/aviron/abakus/controllers/FigureController.java
+++ src/main/java/de/aviron/abakus/controllers/FigureController.java
@@ -8,33 +8,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.aviron.abakus.services.FigureService;
 import de.aviron.abakus.entities.Figure;
 
 @RestController
+@RequestMapping("/abakus/figure")
 public class FigureController {
     
     @Autowired
     private FigureService service;
 
-    @GetMapping(value="/abakus/figure/all")
+    @GetMapping(value="/all")
     List<Figure> getAllCharacters() {
         return service.getAllCharacters();
     }
 
-    @GetMapping(value="/abakus/figure/{id}")
+    @GetMapping(value="/{id}")
     Figure getCharacter(@PathVariable Integer id) {
         return service.getCharacter(id);
     }
 
-    @PostMapping(value="/abakus/figure/add")
+    @PostMapping(value="/add")
     Figure addCharacter(@RequestBody Figure character) {
         return service.addCharacter(character);
     }
 
-    @PutMapping(value="/abakus/figure/set/{id}")
+    @PutMapping(value="/set/{id}")
     Figure setCharacter(@PathVariable Integer id, @RequestBody Figure character) {
         return service.setCharacter(id, character);
     }
