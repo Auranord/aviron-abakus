9b861459da31b0f70adc8c8753f14ab06faabd6a
auranord <aura.nord@googlemail.com>
2022-05-05 16:18:58 +0200

    Updated FigureService to new standard and naming.

--- src/main/java/de/aviron/abakus/services/FigureService.java
+++ src/main/java/de/aviron/abakus/services/FigureService.java
@@ -4,33 +4,38 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import de.aviron.abakus.repositories.FigureRepository;
 import lombok.AllArgsConstructor;
+import de.aviron.abakus.repositories.FigureRepository;
 import de.aviron.abakus.entities.Figure;
 
 @Service
 @AllArgsConstructor
 public class FigureService {
-    
+
     private FigureRepository repository;
 
-    public List<Figure> getAllCharacters() {
+    public List<Figure> getAllFigure() {
         return repository.findAll();
     }
 
-    public Figure getCharacter(Integer id) {
+    public Figure getFigureById(Integer id) {
         return repository.findById(id).orElse(null);
     }
 
-    public Figure addCharacter(Figure character) {
-        return repository.save(character);
+    public Figure addFigure(Figure figure) {
+        return repository.save(figure);
     }
 
-    public Figure setCharacter(Integer id, Figure character) {
-        Figure oldCharacter = repository.findById(id).orElse(null);
-        oldCharacter.setOwner(character.getOwner());
-        oldCharacter.setName(character.getName());
-        return repository.save(oldCharacter);
+    public Figure setFigure(Integer id,Figure figure) {
+        return repository.save(figure);
     }
 
+    public Figure updateFigure(Figure figure) {
+       Figure oldFigure = repository.findById(figure.getId()).orElse(null);
+        if(oldFigure == null)
+            return null;
+            
+        return repository.save(figure);
+    }
+    
 }
