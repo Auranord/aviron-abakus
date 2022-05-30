69bb66b8e28ec03949e0ae241805aab224b7f2bb
GrindingGoat <alex.schertl@web.de>
2022-05-30 22:01:34 +0200

    Message

--- src/main/java/de/aviron/abakus/utils/ControllerUtils.java
+++ src/main/java/de/aviron/abakus/utils/ControllerUtils.java
@@ -4,42 +4,57 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class ControllerUtils {
     
-    // url: http://bbbbbbbbcccccvvvvvvv?key1=x,y,z;key2=y,z;key3=z
+
+    // url: http://bbbbbbbbcccccvvvvvvv?key1=x,y,z&key2=y,z&key3=z
     // return: {"key1": "x,y,z", "key2": "y,z", "key3": "z",}
     public static Map<String, String> getKeysFromUrl(String url) {
+        String [] pairs = url.split("\\?")[1].split(("&"));
+        Map<String, String> map = new HashMap<>();
 
-        // TODO
+        for (String pair : pairs) {
+            if(pair.contains("=")){
+                String [] result = pair.split("=");
 
-        return new HashMap<String, String>();
+                String key = result[0];
+
+                String value = result[1];
+                if(!map.containsKey(key)){
+                    map.put(key, value);
+                }
+            }
+        }
+
+        return map;
     }
+    
 
     // url: http://bbbbbbbbcccccvvvvvvv?key1=x,y,z;key2=y,z;key3=z  / key: "key2"
     // return: {"y", "z"}
     public static List<String> getFieldsFromKey(String url, String key) {
 
-        // TODO
+        Map<String,String> map = getKeysFromUrl(url);
+        ArrayList<String> list = new ArrayList<String>();
+        
+        if(map.containsKey(key)){
+            String [] values = map.get(key).split(",");
+            for (String value : values) {
+                list.add(value);
+            }
+        }
 
-         return new ArrayList<String>();
-    }
-
-    // jsonObject: "{ name: x, email: y, time: z}"  /  field: "email"
-    // return: "{ name: x, time: z}
-    public static String removeFieldFromJson(String jsonObject, String field) {
-
-        // TODO
-
-        return jsonObject;
+        return list;
     }
 
     // jsonObject: "{ name: x, email: y, time: z}"  /  fields: { "name", "email" }
     // return: "{ name: x, email: y}"
-    public static String getFieldsFromJson(String jsonObject, List<String> fields) {
-
-        // TODO
-
+    public static ObjectNode getFieldsFromJson(ObjectNode jsonObject, List<String> fields) {
+        for (String field : fields) {
+            jsonObject.remove(field);
+        }
         return jsonObject;
     }
 
